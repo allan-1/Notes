f0ad9081413c934a1516f500c90f5ad5bee4d9db
Allan Muturi <allan.muturi@elige-africa.com>
2024-03-05 16:12:19 +0300

    feat: add welcome screen

---
+++ Notes/view/Welcome.swift
@@ -0,0 +1,33 @@
+//
+//  Welcome.swift
+//  Notes
+//
+//  Created by Allan on 3/5/24.
+//
+
+import SwiftUI
+
+struct Welcome: View {
+    @State private var name: String = ""
+    @FocusState private var emailFieldIsFocused: Bool
+    
+    var body: some View {
+        VStack{
+            Image("book-notes").aspectRatio(contentMode: .fit)
+            Text("This notes belongs to").font(.system(size: 16, weight: .regular, design: .monospaced)).padding()
+            TextField("What's your name?", text: $name).focused($emailFieldIsFocused) .textInputAutocapitalization(.never)
+                .disableAutocorrection(true).textFieldStyle(.automatic).padding()
+            Button(action: continueToHome){
+                Text("Start")
+            }.buttonStyle(.borderedProminent).padding()
+        }
+    }
+}
+
+func continueToHome(){
+    
+}
+
+#Preview {
+    Welcome()
+}
